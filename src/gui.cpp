af42863313db70894275c409f31431e746bdf6b3
someotb <llyryk934@gmail.com>
2026-04-07 11:19:09 +0700

    feat(gui): add IP bridge, and refactor PHY bridge

--- src/gui.cpp
+++ src/gui.cpp
@@ -1,31 +1,79 @@
-#include "common.hpp"
-#include "logger.hpp"
+#include "sockets.hpp"
 #include "gui/app.hpp"
 
+#include <functional>
+#include <string>
 #include <thread>
+#include <vector>
 
 float lat = 1.0f;
+static std::string str_pid;
 
-int run_bridge(Buffers &bufs)
+int run_ip_brigde(Buffers &data, socketData &socket)
 {
-    static IPC client;
-    static ipc_header h;
+    while (socket.ip_socket.empty())
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    static std::string last_connected_path = "";
+    IPC client;
+    ipc_header header;
     static bool init = false;
+    static ImPlotSpec specs;
+    std::vector<uint8_t> bytes;
+
+    while (true)
+    {
+        if (socket.ip_socket != last_connected_path && !socket.ip_socket.empty())
+        {
+            logs::gui.info("Bridge connecting to: {}", socket.ip_socket);
+            client.connect_to(socket.ip_socket);
+            last_connected_path = socket.ip_socket;
+        }
+
+        if (last_connected_path.empty()) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            continue;
+        }
+
+        if (client.recv_frame(header, bytes))
+            data.ip.write(bytes);
+    }
+
+    return 0;
+}
+
+int run_dsp_bridge(Buffers &bufs, socketData &socket)
+{
+    while (socket.phy_socket.empty())
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    IPC client;
+    static std::string last_connected_path = "";
+    ipc_header h;
     std::vector<int16_t> raw(1920 * 2, 0);
     std::vector<std::complex<float>> symbols;
 
     while (true)
     {
-        if (!init)
+        if (socket.phy_socket != last_connected_path && !socket.phy_socket.empty())
         {
-            while (client.connect_to_socket("/tmp/dsp_gui.sock") == -1)
-                std::this_thread::sleep_for(std::chrono::milliseconds(10));
-            init = true;
+            logs::gui.info("Bridge connecting to: {}", socket.phy_socket);
+            client.connect_to(socket.phy_socket);
+            last_connected_path = socket.phy_socket;
         }
-        init = client.recv_frame(h, symbols);
-        lat = 0.001 * (client.now_ns() - h.timestamp_ns) + 0.999 * lat;
-        bufs.sdr_raw.write(raw);
-        bufs.dsp.write(symbols);
+
+        if (last_connected_path.empty()) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            continue;
+        }
+
+        if (client.recv_frame(h, symbols))
+        {
+            logs::gui.info("RECIEVED frame from SDR, size: {}", symbols.size());
+            lat = 0.001 * (client.now_ns() - h.timestamp_ns) + 0.999 * lat;
+            bufs.sdr_raw.write(raw);
+            bufs.dsp.write(symbols);
+        }
     }
 
     return 0;
@@ -33,10 +81,19 @@ int run_bridge(Buffers &bufs)
 
 int main()
 {
+    std::vector<std::string> all_sockets;
+    found_sockets(all_sockets);
     Buffers bufs(1920 * 2);
-    std::thread bridge(run_bridge, std::ref(bufs));
-    std::thread gui(run_gui, std::ref(bufs));
+
+    socketData sock(false);
+
+    std::thread gui(run_gui, std::ref(bufs), std::ref(all_sockets), std::ref(sock));
+    std::thread phy_bridge(run_dsp_bridge, std::ref(bufs), std::ref(sock));
+    std::thread ip_bridge(run_ip_brigde, std::ref(bufs), std::ref(sock));
+
     gui.join();
-    bridge.join();
+    ip_bridge.join();
+    phy_bridge.join();
+
     return 0;
 }
