73d3f5c93376553eb34fbd943232d361523e7f1b
someotb <llyryk934@gmail.com>
2026-04-07 11:25:40 +0700

    feat(gui): use safe data display and clean up code

--- include/phy/phy_layer.hpp
+++ include/phy/phy_layer.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
 #include "common.hpp"
+#include "sockets.hpp"
 
 #include <SoapySDR/Device.hpp>
 
 int run_sdr(SharedData &data);
 
-int run_dsp_gui_bridge(SharedData &data);
+int run_dsp_gui_bridge(SharedData &data, socketData &socket);
--- src/gui/phy_dev.cpp
+++ src/gui/phy_dev.cpp
@@ -1,41 +1,35 @@
 #include "gui/phy_dev.hpp"
+#include "imgui.h"
 
 #include <vector>
-#include "implot.h"
 
 extern float lat;
 
-void phy_dev(App &app, Buffers &data) // Phy layer
-{
-    static std::vector<std::complex<float>> buffer(1920);
-    static ImGuiIO &io = ImGui::GetIO();
-    static ImPlotSpec specs;
-    static ImPlotSpec specs2;
-    static std::string label = "Raw signal";
-    static bool init = false;
-    if (!init)
-    {
-        specs.Stride = sizeof(float);
-        specs.Offset = 0;
-        specs2.Stride = sizeof(float) * 2;
-        specs2.Offset = 0;
-        specs.Marker = ImPlotMarker_Asterisk;
-        specs.MarkerSize = 2.0f;
-        init = true;
-    }
+void phy_dev(App &app, Buffers &data) {
+    static std::vector<std::complex<float>> buffer;
 
     data.dsp.read(buffer);
-    auto size = buffer.size() / 2;
-    const float *raw_ptr = reinterpret_cast<const float *>(buffer.data());
 
     if (ImGui::Begin("Phy"))
     {
-        ImGui::Text("Latency  %.2f mcs | %.2f ms", lat / 1e3, lat / 1e6);
-        ImGui::Text("FPS: %.1f (%.3f ms)", io.Framerate, 1000.0f / io.Framerate);
-        app.begin_scatter<float, std::complex<float>>(label, buffer);
+        if (!buffer.empty())
+        {
+            ImGui::Text("Latency  %.2f mcs | %.2f ms", lat / 1e3, lat / 1e6);
+            ImGui::SameLine();
+            ImGui::Text("Buffer size: %zu", buffer.size());
+            app.begin_scatter<float, std::complex<float>>("Raw I/Q", buffer);
+        }
+        else
+            ImGui::Text("Waiting for data...");
     }
     ImGui::End();
+
     if (ImGui::Begin("Line"))
-        app.begin_plot_2d<float, std::complex<float>>(label, "I", "Q", buffer);
+    {
+        if (!buffer.empty())
+            app.begin_plot_2d<float, std::complex<float>>("Raw samples", "I", "Q", buffer);
+        else
+            ImGui::Text("Waiting for data...");
+    }
     ImGui::End();
 }
